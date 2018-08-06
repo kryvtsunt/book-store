f33cc1a5136f9127cf3837373e7e0dc9221a8292
Tymofii Kryvtsun <kryvtsun.t@husky.neu.edu>
2018-08-06 14:00:01 -0400

    errors fixed

--- backend.py
+++ backend.py
@@ -8,7 +8,6 @@ class Database:
         self.cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
         self.conn.commit()
 
-
     def insert(self, title,author,year,isbn):
         self.cur.execute("INSERT INTO book VALUES (NULL,?,?,?,?)",(title,author,year,isbn))
         self.conn.commit()
